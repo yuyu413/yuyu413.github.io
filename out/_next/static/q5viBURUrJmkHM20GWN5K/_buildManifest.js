ee4b8eb1e0ed42490f7cac98a836085542352798
yuyu413 <yuyu413@naver.com>
2023-12-06 11:54:14 +0900

    out test

---
+++ out/_next/static/q5viBURUrJmkHM20GWN5K/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/css/a1802a52603bcb96.css","static/chunks/pages/index-27647224ff8190b6.js"],"/_error":["static/chunks/pages/_error-ee5b5fb91d29d86f.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
